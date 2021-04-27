46b6ed2e408bd29a762b1469f78f114d49b7039a
Kfir Cohen <cokfi@post.bgu.ac.il>
2021-04-27 20:04:58 +0300

    A 0 into Z

--- LAB3 task/DUT/Datapath.vhd
+++ LAB3 task/DUT/Datapath.vhd
@@ -18,12 +18,39 @@ entity Datapath is
 end Datapath;
 ------------- complete the Datapath Unit Architecture code --------------
 architecture arc_sys of Datapath is
-signal reg_c,reg_b:STD_LOGIC_VECTOR(n-1 downto 0); 
-signal reg_opc:STD_LOGIC_VECTOR(2 downto 0); 
+signal reg_c,reg_b,ALUout:STD_LOGIC_VECTOR(n-1 downto 0); 
+signal reg_opc,ALUFN:STD_LOGIC_VECTOR(2 downto 0); 
+signal enable_dec,A std_logic;
+alias Opcode :std_logic_vector(2 downto 0) is DATAin(2 downto 0);
+);
 begin
 	One<= and DATAin ;
-
-
+	ALUFN<= reg_opc when(OPC2='1') else "ZZZ";
+	A<='1' when (OPC1='1') else 'Z';
+	
+------------------------counter process--------------------------
+d_counter : process(clk)
+begin
+	if (clk'event and clk='1') then -- rising edge
+		if (Ld ='1') then
+			counter <=DATAin;
+		elsif (Ld ='0') then
+			counter <= counter -1;
+		end if;	
+	end if;
+end process;
+------------------------Registers process--------------------------
+reg_opc_proc : process(clk)
+begin
+	if (clk'event and clk='1') then -- rising edge
+		if (OPCin='1')
+			reg_opc <=Opcode;
+		end if;
+		if (Bin='1')
+			reg_b <=ALUout;
+	end if;
+	end if;
+end process;
 	
 
 
