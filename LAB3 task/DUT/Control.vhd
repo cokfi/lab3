5a51af73d573597b435e688ed199d8844279746b
Kfir Cohen <cokfi@post.bgu.ac.il>
2021-04-28 13:24:31 +0300

    modify ron's new files

--- LAB3 task/DUT/Control.vhd
+++ LAB3 task/DUT/Control.vhd
@@ -7,23 +7,99 @@ entity Control is
 	port(
 		rst,clk : in std_logic;
 		Input,One : in std_logic;
-		---------------------------------------------
+		
 		OPCin,OPC2,OPC1,Ld,Bin,Cout : out std_logic
 	);
 end Control;
 ------------- complete the Control Unit Architecture code --------------
 architecture arc_sys of Control is
-
-	
+	type state is (Idle,SetN,First,Operate,Last,Done); 
+	signal pr_state,nx_state: state;
 begin
-	
+----Lower Section:-------------
+	process(rst,clk)
+	begin
+		if (rst='1') then
+			pr_state <= Idle;
+		elsif (clk'event and clk='1') then
+			pr_state <= nx_state;
+		end if;
+	end process
+----Upper Section:-------------
+	process(Input,One,pr_state)
+	begin 
+			case pr_state is 
+				when Idle =>
+					OPCin<='1';
+					OPC2<='0';
+					OPC1<='0';
+					Ld<='1';
+					Bin<='0';
+					Cout<='0';
+					if (Input='1') then
+						nx_state <= SetN;	
+					else
+						nx_state <= Idle;
+					end if;
 				
-	
-	
-	
-	
-	
-	
+				when SetN =>
+					OPCin='0';
+					OPC2='0';
+					OPC1='0';
+					Ld='1';
+					Bin='0';
+					Cout='0';
+					nx_state <= First;
+							
+				when First =>
+					OPCin='0';
+					OPC2='0';
+					OPC1='1';
+					Ld='0';
+					Bin='1';
+					Cout='0';
+					if (One='0') then
+						nx_state<=Operate;
+					else
+						nx_state<=Last;
+					end if;
+
+				when Operate =>
+					OPCin='0';
+					OPC2='1';
+					OPC1='0';
+					Ld='0';
+					Bin='1';
+					Cout='0';
+					if (One='0') then
+						nx_state<=Operate;
+					else
+						nx_state<=Last;
+					end if;
+				
+                    when Last => 
+                    OPCin<='0';
+					OPC2<='1';
+					OPC1<='0';
+					Ld<='0';
+					Bin<='1';
+					Cout<='0';
+                    nx_state <= Done;
+
+                    when Done =>
+					OPCin<='1';
+					OPC2<='0';
+					OPC1<='0';
+					Ld<='0';
+					Bin<='0';
+					Cout<='1';
+					if (Input='1') then
+						nx_state<=SetN;
+					else
+						nx_state<=Idle;
+					end if;
+			end case;
+	end process
 end arc_sys;
 
 
--- LAB3 task/DUT/Logical.vhd
+++ LAB3 task/DUT/Logical.vhd
@@ -3,7 +3,8 @@ use ieee.std_logic_1164.all;
 USE work.aux_package.all;
 ENTITY Logical IS
   GENERIC ( n : INTEGER := 8);     
-  PORT (    ALUFN: IN STD_LOGIC_VECTOR (1 DOWNTO 0); --Selection := [0,1]= or,[1,0] = and, [1,1] = xor  
+  PORT (    ALUFN: IN STD_LOGIC_VECTOR (1 DOWNTO 0); --ALUFN=[ALUFN(2),ALUFN(0)]
+                                                     --Selection := [0,1]= or,[1,0] = and, [1,1] = xor  
 			y: IN STD_LOGIC_VECTOR (n-1 DOWNTO 0); -- Input
             x: IN STD_LOGIC_VECTOR (n-1 DOWNTO 0); -- Input
             result: OUT STD_LOGIC_VECTOR(n-1 downto 0)); -- Result
@@ -13,10 +14,7 @@ ARCHITECTURE dfl OF Logical IS
 begin
 -- The boolean Logic is in Documentation
     loop1: for i in 0 to n-1 generate
-        result(i)<= ((ALUFN(1) and ALUFN(0)) and (y(i) nor x(i))) or
-        ((ALUFN(1) and not(ALUFN(0))) and (y(i) xor x(i))) or
-        ((not(ALUFN(1)) and ALUFN(0)) and (y(i) and x(i))) or
-        ((ALUFN(1) nor ALUFN(0)) and (y(i) or x(i)));
+        result(i)<= (ALUFN(0) and not(x(i)) and y(i)) or (x(i) and(not(ALUFN(1)) or (ALUFN(2) and (ALUFN(0) xor y(i)))));
         end generate;
 end dfl;
 
