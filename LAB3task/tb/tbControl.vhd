7d0244c4bf7823379508785e3781f46e0192a4f3
Kfir Cohen <cokfi@post.bgu.ac.il>
2021-05-03 13:55:20 +0300

    tb_control is fixed

--- LAB3task/tb/tbControl.vhd
+++ LAB3task/tb/tbControl.vhd
@@ -33,8 +33,10 @@ begin
 
         SwitchInput : process
         begin
+            Input <= 'U';
+            wait for 12.5 ns;--reading starts
             Input <= '0';
-            wait for 62.5 ns;-- trigR's raising edge
+            wait for 50 ns;-- trigR's raising edge
             Input <= '1'; 
             wait for 500 ns;--562.5ns
             Input <= '0';
@@ -48,6 +50,12 @@ begin
             One <= '0';
             wait for 312.5 ns;--325 ns falling edge
             One <= '1';
+            wait for 50 ns; --375 ns
+            One <= '0';
+            wait for 100 ns;--475
+            One <= '1';
+            wait for 50 ns; --525 ns
+            One <= '0';
             wait;
         end process;
 end ctb ;
